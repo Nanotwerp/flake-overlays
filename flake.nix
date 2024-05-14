bc03ebe90ff6d0cb49d0b879d62a59057d63ec14
Nano Twerpus <nanotwerp@gmail.com>
2024-05-14 04:33:28 -0400

    Update

--- flake.nix
+++ flake.nix
@@ -29,51 +29,7 @@
           };
         in
         let
-          fd-nightly = rustPlatform.buildRustPackage
-            rec {
-              pname = "fd";
-              version = "10.1.0";
-
-              src = pkgs.fetchFromGitHub {
-                owner = "sharkdp";
-                repo = "fd";
-                rev = "v${version}";
-                hash = "sha256-9fL2XV3Vre2uo8Co3tlHYIvpNHNOh5TuvZggkWOxm5A=";
-              };
-              cargoHash = "sha256-3TbsPfAn/GcGASc0RCcyAeUiD4RUtvTATdTYhKdBxvo=";
-
-              nativeBuildInputs = [ nixpkgs.installShellFiles pkgs.mold ];
-
-              buildInputs = with pkgs; [ rust-jemalloc-sys ];
-
-              postInstall = ''
-                installManPage doc/fd.1
-
-                installShellCompletion --cmd fd \
-                  --bash <($out/bin/fd --gen-completions bash) \
-                  --fish <($out/bin/fd --gen-completions fish)
-                installShellCompletion --zsh contrib/completion/_fd
-              '';
-
-              passthru.tests.version = nixpkgs.testers.testVersion {
-                package = pkgs.fd;
-              };
-
-              meta = with nixpkgs.lib; {
-                description = "A simple, fast and user-friendly alternative to find";
-                longDescription = ''
-                  `fd` is a simple, fast and user-friendly alternative to `find`.
-
-                  While it does not seek to mirror all of `find`'s powerful functionality,
-                  it provides sensible (opinionated) defaults for 80% of the use cases.
-                '';
-                homepage = "https://github.com/sharkdp/fd";
-                changelog = "https://github.com/sharkdp/fd/blob/v${version}/CHANGELOG.md";
-                license = with licenses; [ asl20 /* or */ mit ];
-                maintainers = with maintainers; [ dywedir figsoda globin ma27 zowoq ];
-                mainProgram = "fd";
-              };
-            };
+          fd-nightly = pkgs.callPackage ./packages/fd/package.nix;
         in
         {
           inherit fd-nightly;
